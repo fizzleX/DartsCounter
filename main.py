aea6b3e8a643e230316b396c7545a1d09106c7e0
Marco Rudolph <shagjii@web.de>
2021-02-11 23:41:44 +0100

    CalcAverageLeg Function

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 from tkinter import *
+
+class Player():
+    points = 0 
+    def __init__(self):        
+        self.average = 0.0
+        self.averageLeg = 0.0
+        self.shots = 0
+        self.shotsLeg = 0
+    
+    def CalcAverageLeg(self, points):
+        if self.averageLeg != 0.0:
+            self.averageLeg = (self.averageLeg + points) / 2
+        else:
+            self.averageLeg += points
+        return
+    
+    def CalcAverage(self, points):
+        return
+    
+player1 = Player()
+player1.CalcAverageLeg(100)
+print(player1.averageLeg)
+player1.CalcAverageLeg(50)
+print(player1.averageLeg)
