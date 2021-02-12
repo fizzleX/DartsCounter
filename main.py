fdbc25567c32e3ab4a8c8484d346a4b38e9418aa
Marco Rudolph <shagjii@web.de>
2021-02-12 11:27:58 +0100

    Calc average

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 from tkinter import *
 
 class Player():
-    points = 0 
     shotsFactor = 1 / 3
     
-    def __init__(self):        
+    def __init__(self):    
+        self.points = 0 
+        self.pointsLeg = 0
+        self.pointsLeft = 0    
         self.average = 0.0
         self.averageLeg = 0.0
         self.shots = 0
-        self.shotsLeg = 0
+        self.shotsLeg = 0        
     
+    # Updates the values after new points given
     def UpdateValues(self, points):
+        self.pointsLeft -= points
         self.points += points
+        self.pointsLeg += points
         self.shots += self.shotsFactor
         self.shotsLeg += self.shotsFactor
         self.CalcAverageLeg()
-        #CalcAverage(points)
+        self.CalcAverage()
     
+    # calculates the average of the leg
     def CalcAverageLeg(self):
-        self.averageLeg = self.points / self.shotsLeg
-           
-    def CalcAverage(self, points):        
-        return
+        self.averageLeg = self.pointsLeg / self.shotsLeg
     
-    def NewLeg(self):
+    # calculates the average of the total match       
+    def CalcAverage(self):
+        self.average = self.points / self.shots                
+    
+    # starts a new leg
+    def NewLeg(self, points):
+        self.pointsLeft = points
+        self.pointsLeg = 0
         self.averageLeg = 0.0
         self.shotsLeg = 0
     
 player1 = Player()
-player1.UpdateValues(60)
-print(player1.averageLeg)
-player1.UpdateValues(60)
-print(player1.averageLeg)
-player1.UpdateValues(20)
-print(player1.averageLeg)
-player1.UpdateValues(21)
-print(player1.averageLeg)
-player1.UpdateValues(34)
-print(player1.averageLeg)
-player1.UpdateValues(27)
-print(player1.averageLeg)
+player2 = Player()    
+
